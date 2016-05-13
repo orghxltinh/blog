c99645e5ab12a3df1861a344c10b82db85a5c2a9
tinh ha <hxltinh@gmail.com>
2016-05-13 08:44:39 +0700

    update

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -54,7 +54,7 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
       '__apiHostName__': JSON.stringify('http://localhost'),
-      '__apiPort__': JSON.stringify(8080)
+      '__apiPort__': JSON.stringify(80)
     }),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
